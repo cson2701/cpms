0fb3b2726d327031e92b3eb91e2423bd6d795b7b
song_cr <cson2701@uni.sydney.edu.au>
2020-04-04 20:17:16 +1100

    initial scr

---
+++ src/test/java/com/cs68_4/cpms/CpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs68_4.cpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
